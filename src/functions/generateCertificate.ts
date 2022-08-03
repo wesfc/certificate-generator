9f893022eccd1b763a9072c9b0494f8fd0052c5d
Wesley Carvalho <wesleyfcarvalho1@gmail.com>
2022-08-03 15:52:09 -0300

    Generate PDF file based on data received

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,12 +1,33 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from '../utils/dynamodbClient'
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { compile } from 'handlebars';
+import dayjs from 'dayjs';
+import chromium from 'chrome-aws-lambda';
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
- 
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+}
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
@@ -28,6 +49,35 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     }
   }).promise();
 
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data:ITemplate = {
+    id,
+    name,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal
+  }
+  const content = await compileTemplate(data);
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? "./certificate.pdf" : null,
+  })
   return {
     statusCode: 201,
     body: JSON.stringify(response.Items[0]),
